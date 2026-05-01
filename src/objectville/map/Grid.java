c81860f25280c0ee3a6a0ed957183c7d9bf67d12
aasliarass <arasasli7180@gmail.com>
2026-05-01 23:07:14 +0300

    Implement initial Position and Grid logic

--- src/objectville/map/Grid.java
+++ src/objectville/map/Grid.java
@@ -1,4 +1,39 @@
 package objectville.map;
 
 public class Grid {
+
+    private int rows;
+    private int cols;
+    private char[][] cells;
+
+    public Grid(int rows, int cols){
+        this.rows = rows;
+        this.cols = cols;
+        cells = new char[rows][cols];
+    }
+
+    public int getRows(){
+        return rows;
+    }
+
+    public int getCols(){
+        return cols;
+    }
+
+    public boolean isValidPosition(int row, int col){
+        return row >= 0 && col >= 0 && rows > row && cols > col;
+    }
+
+    public void setCell(int row, int col, char value){
+        if(isValidPosition(row, col)){
+            cells[row][col] = value;
+        }
+    }
+    public char getCell(int row, int col){
+        if(isValidPosition(row,col)){
+            return cells[row][col];
+        }else{
+            return '\0';
+        }
+    }
 }
--- src/objectville/map/Position.java
+++ src/objectville/map/Position.java
@@ -1,4 +1,25 @@
 package objectville.map;
 
 public class Position {
+
+    private int row;
+    private int col;
+
+    public Position(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getCol(){
+        return col;
+    }
+
+    @Override
+    public String toString(){
+        return "("+row+","+col+")";
+    }
 }
